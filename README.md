5808341995b4c2d39f4b0fec5c624c9724d36259
Nate <nzurbrugg@gmail.com>
2017-08-05 21:35:11 -0400

    New bitcoinaverage api and updated chart image

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# BitcoinLogLogChart
+# Bitcoin Log Log Chart
+
+![Chart image](https://github.com/nzurbrugg/BitcoinLogLogChart/raw/master/chart.png)
+
+This is a log-log plot of the bitcoin price history using data from bitcoinaverage.com and the matplotlib python library.  The date of the genesis block (ie the first mined block) is used as time 0 for log scaling the date axis.
--- plot.py
+++ plot.py
@@ -7,25 +7,31 @@ from matplotlib.ticker import FuncFormatter
 from math import log10
 import urllib
 
-urllib.urlretrieve("https://api.bitcoinaverage.com/history/USD/per_day_all_time_history.csv", 'per_day_all_time_history.csv')
-data = pandas.read_csv('per_day_all_time_history.csv')
-date = data.DateTime.tolist()
-average = data.Average.tolist()
-low = data.Low.tolist()
-high = data.High.tolist()
+import hashlib
+import hmac
+import requests
+import time
+
+secret_key = input('Enter your secret key: ')
+public_key = input('Enter your public key: ')
+
+timestamp = int(time.time())
+payload = '{}.{}'.format(timestamp, public_key)
+hex_hash = hmac.new(secret_key.encode(), msg=payload.encode(), digestmod=hashlib.sha256).hexdigest()
+signature = '{}.{}'.format(payload, hex_hash)
+
+url = 'https://apiv2.bitcoinaverage.com/indices/global/history/BTCUSD?period=alltime&?format=csv'
+headers = {'X-Signature': signature}
+result = requests.get(url=url, headers=headers)
+
+data = pandas.read_json(result.text)
+date = data.time.tolist()
+average = data.average.tolist()
+low = data.low.tolist()
+high = data.high.tolist()
 
 genesisdate = '1/3/2009'
 daysFromGenesis = mdates.datestr2num(date) - mdates.datestr2num(genesisdate)
-
-fig, ax = plt.subplots()
-#ax.loglog(daysFromGenesis, low, linewidth=1, color='0.5')
-#ax.loglog(daysFromGenesis, high, linewidth=1, color='0.5')
-averagePlot = ax.loglog(daysFromGenesis, average, linewidth=1, label='Daily Average Price')
-
-ax.set_ylabel('$/BTC')
-
-years = mdates.YearLocator()
-months = mdates.MonthLocator()
 
 def datestr2x(datestr):
     x = mdates.datestr2num(datestr) - mdates.datestr2num(genesisdate)
@@ -41,18 +47,8 @@ def yLogFormat(y,pos):
     formatstring = '{{:.{:1d}f}}'.format(decimalplaces)
     return formatstring.format(y)
 
-ax.xaxis.set_major_locator(years)
-ax.xaxis.set_major_formatter(FuncFormatter(xMajorFormat))
-ax.xaxis.set_minor_locator(months)
-
 datemin = datestr2x('6/1/2010')
 datemax = datestr2x('1/1/2024')
-ax.set_xlim(datemin, datemax)
-
-ax.yaxis.set_major_formatter(FuncFormatter(yLogFormat))
-ymin = 0.01
-ymax = 40000
-ax.set_ylim(ymin, ymax)
 
 def plotLineFrom2Points(date1, price1, date2, price2):
     x1=datestr2x(date1)
@@ -67,6 +63,25 @@ def plotLineFrom2Points(date1, price1, date2, price2):
 
     return line
 
+years = mdates.YearLocator()
+months = mdates.MonthLocator()
+
+ax = plt.subplot(1,1,1)
+#ax.loglog(daysFromGenesis, low, linewidth=1, color='0.5')
+#ax.loglog(daysFromGenesis, high, linewidth=1, color='0.5')
+averagePlot = ax.loglog(daysFromGenesis, average, linewidth=1, label='Daily Average Price')
+
+ax.xaxis.set_major_locator(years)
+ax.xaxis.set_major_formatter(FuncFormatter(xMajorFormat))
+ax.xaxis.set_minor_locator(months)
+ax.set_xlim(datemin, datemax)
+
+ax.set_ylabel('$/BTC')
+ax.yaxis.set_major_formatter(FuncFormatter(yLogFormat))
+ymin = 0.01
+ymax = 40000
+ax.set_ylim(ymin, ymax)
+
 ax.grid(b=True, which='major', color='0.8', linestyle='-')
 ax.grid(b=True, which='minor', color='0.96', linestyle='-')
 ax.set_axisbelow(True)
@@ -76,30 +91,13 @@ plt.axvline(x=datestr2x('11/28/2012'), color='r', linestyle="--", label='Mining 
 plt.axvline(x=datestr2x('7/10/2016'), color='r', linestyle="--")
 plt.axvline(x=datestr2x('3/10/2020'), color='r', linestyle="--")
 plt.axvline(x=datestr2x('12/10/2023'), color='r', linestyle="--")
-###peak dates
-
-#new high peaks
-#plt.axvline(x=datestr2x('11/7/2010'), linestyle="--")
-#plt.axvline(x=datestr2x('2/13/2011'), linestyle="--")
-#plt.axvline(x=datestr2x('6/9/2011'), linestyle="--")
-#plt.axvline(x=datestr2x('4/9/2013'), linestyle="--")
-#plt.axvline(x=datestr2x('11/29/2013'), linestyle="--")
-
-#rebound peaks
-#plt.axvline(x=datestr2x('1/8/2012'), linestyle="--")
-#plt.axvline(x=datestr2x('8/17/2012'), linestyle="--")
-
-#weak rebound peaks
-#plt.axvline(x=datestr2x('7/1/2014'), linestyle="--", color='0.8')
-#plt.axvline(x=datestr2x('11/14/2014'), linestyle="--", color='0.8')
-#plt.axvline(x=datestr2x('11/4/2015'), linestyle="--", color='0.8')
-
-#predicted peaks
-#plt.axvline(x=datestr2x('2/1/2017'), linestyle="--", color='0.8')
-#plt.axvline(x=datestr2x('4/1/2018'), linestyle="--", color='0.8')
 
 #low boundary line
 plotLineFrom2Points('12/10/2010',0.19,'5/23/2016',443.77)
+#plotLineFrom2Points('12/10/2010',0.22,'9/15/2015',228)
+
+#low average line
+#plotLineFrom2Points('12/10/2010',0.3,'5/23/2016',570)
 
 #low peak line
 #plt.plot([1.5*365, 15*365], [0.2, 50000], color='k', linestyle=':')
